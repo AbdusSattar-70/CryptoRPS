f7c3e3106d6ccc79675a97e61851734a1f75e3b5
Abdus Sattar <sasorganicagro@gmail.com>
2023-12-05 19:13:12 +0600

    Add InvalidInputError.js class

---
+++ InvalidInputError.js
@@ -0,0 +1,9 @@
+
+class InvalidInputError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = 'InvalidInputError';
+    }
+}
+
+export default InvalidInputError;
