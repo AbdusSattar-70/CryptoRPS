5e7d7d35e662bb482c02e7bfa0e109e38bd6c166
Abdus Sattar <sasorganicagro@gmail.com>
2023-12-05 19:10:51 +0600

    Add gameLogic class

---
+++ gameLogic.js
@@ -0,0 +1,34 @@
+import chalk from 'chalk'
+import InvalidInputError from './InvalidInputError.js';
+class GameLogic {
+    constructor(moves) {
+        this.moves = moves;
+    }
+
+    determineWinner(userMove, computerMove) {
+        const n = this.moves.length;
+        const half = Math.floor(n / 2);
+        const userIndex = this.moves.indexOf(userMove);
+        const computerIndex = this.moves.indexOf(computerMove);
+
+        if (userIndex === -1 || computerIndex === -1) {
+            throw new InvalidInputError(chalk.red('Invalid move: Please choose a valid move by entering an Available number'));
+
+        }
+
+        const winningMoves = this.moves.slice(computerIndex + 1, computerIndex + 1 + half)
+            .concat(this.moves.slice(0, computerIndex - half));
+
+        if (userMove === computerMove) {
+            return chalk.yellow('It\'s a draw!');
+        }
+         else if (winningMoves.includes(userMove)) {
+            return chalk.green('You won!');
+
+        } else {
+            return chalk.red('Computer won!');
+        }
+    }
+}
+
+export default GameLogic;
